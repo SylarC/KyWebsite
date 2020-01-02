4649661eee122342bd71e131c28e4978f9fe9866
SylarC <sylar.cavolta@gmail.com>
2020-01-02 14:23:51 -0800

    Fixed graphic issues

--- robotics.php
+++ robotics.php
@@ -32,6 +32,7 @@
     </nav>
 </div>
 
+<div class="blog">
 <!-- First Container -->
 <div class="header">
     <h2>Robotics with Shoob</h2>
@@ -49,38 +50,10 @@
         <a class="card">
             <h2>About Me</h2>
             <img src="kyShoob.jpg" alt="kyShoob.jpg" width="100" height="100">
-            <p>Hello! I am Shoob, and I am here to keep you updated on Subzero Robotics</p>
+            <p>Hello! I am Shoob, here to keep you updated on Subzero Robotics</p>
         </div>
     </div>
-<!--
-Second Container:
-    <div class="container-fluid bg-2 text-center">
-        <h3 class="margin">What Am I?</h3>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
-        <a href="#" class="btn btn-default btn-lg">
-            <span class="glyphicon glyphicon-search"></span> Search
-        </a>
-    </div>
-Third Container (Grid):
-    <div class="container-fluid bg-3 text-center">
-        <h3 class="margin">Where To Find Me?</h3><br>
-        <div class="row">
-            <div class="col-sm-4">
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                <img src="birds1.jpg" class="img-responsive margin" style="width:100%" alt="Image">
-            </div>
-            <div class="col-sm-4">
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                <img src="birds2.jpg" class="img-responsive margin" style="width:100%" alt="Image">
-            </div>
-            <div class="col-sm-4">
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                <img src="birds3.jpg" class="img-responsive margin" style="width:100%" alt="Image">
-            </div>
-        </div>
-    </div>
--->
-
+</div>
 <!-- Footer -->
 <footer class="container-fluid bg-4 text-center">
     <p>Website by Ky, theme from <a href="https://www.w3schools.com">www.w3schools.com</a></p>
