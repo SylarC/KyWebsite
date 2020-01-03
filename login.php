59fec4951bbb82f6d0e14a8dd7ae8d485f842b87
Kytis Hong <Ky@KyMacbookAir.local>
2020-01-02 18:21:34 -0800

    Split CSS + split files in to directories

--- login.php
+++ login.php
@@ -6,7 +6,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="css/loginstyles.css">
     <link href="https://fonts.googleapis.com/css?family=Comfortaa&display=swap" rel="stylesheet">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
@@ -21,8 +22,8 @@
             </div>
             <ul class="nav navbar-nav">
                 <li><a href="index.html">Home</a></li>
-                <li><a href="robotics.php">Robotics</a></li>
-                <li><a href="pictures.php">Pictures</a></li>
+                <li><a href="robotics.html">Robotics</a></li>
+                <li><a href="pictures.html">Pictures</a></li>
                 <li><a href="followme.html">Follow Me!</a></li>
             </ul>
             <ul class="nav navbar-nav navbar-right">
@@ -35,7 +36,7 @@
 <!-- First Container -->
 <h2>Login Form</h2>
 
-<form action="loginaction.php" method="post">
+<form action="php/loginaction.php" method="post">
     <div class="container">
         <label for="user"><b>Username</b></label>
         <input type="text" placeholder="Enter Username" name="user" required>
