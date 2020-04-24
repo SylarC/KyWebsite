93bf4ee3a1395d8a76c85d4bcbfe47fb8d8abfc7
SylarC <sylar.cavolta@gmail.com>
2020-04-24 16:39:14 -0700

    Added chat erase after closing page

--- Chat/index.php
+++ Chat/index.php
@@ -1,6 +1,7 @@
 <?php
-session_start ();
-function loginForm() {
+session_start();
+function loginForm()
+{
     echo '
     <div id="loginform">
     <form action="index.php" method="post">
@@ -13,31 +14,32 @@ function loginForm() {
     ';
 }
 
-if (isset ( $_POST ['enter'] )) {
+if (isset ($_POST ['enter'])) {
     if ($_POST ['name'] != "") {
-        $_SESSION ['name'] = stripslashes ( htmlspecialchars ( $_POST ['name'] ) );
-        $fp = fopen ( "log.html", 'a' );
-        fwrite ( $fp, "<div class='msgln'><i>User " . $_SESSION ['name'] . " has joined the chat session.</i><br></div>" );
+        $_SESSION ['name'] = stripslashes(htmlspecialchars($_POST ['name']));
+        $fp = fopen("log.html", 'a');
+        fwrite($fp, "<div class='msgln'><i>User " . $_SESSION ['name'] . " has joined the chat session.</i><br></div>");
         ftruncate($fp, 0);
     } else {
         echo '<span class="error">Please type in a name</span>';
     }
 }
 
-if (isset ( $_GET ['logout'] )) {
+if (isset ($_GET ['logout'])) {
 
     // Simple exit message
-    $fp = fopen ( "log.html", 'a' );
-    fwrite ( $fp, "<div class='msgln'><i>User " . $_SESSION ['name'] . " has left the chat session.</i><br></div>" );
+    $fp = fopen("log.html", 'a');
+    fwrite($fp, "<div class='msgln'><i>User " . $_SESSION ['name'] . " has left the chat session.</i><br></div>");
     fwrite($fp, "");
-    fclose ( $fp );
+    fclose($fp);
 
-    session_destroy ();
-    header ( "Location: index.php" ); // Redirect the user
+    session_destroy();
+    header("Location: index.php"); // Redirect the user
 }
 
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
     <style>
@@ -48,7 +50,7 @@ if (isset ( $_GET ['logout'] )) {
             padding: 35px;
         }
 
-        form,p,span {
+        form, p, span {
             margin: 0;
             padding: 0;
         }
@@ -66,7 +68,7 @@ if (isset ( $_GET ['logout'] )) {
             text-decoration: underline;
         }
 
-        #wrapper,#loginform {
+        #wrapper, #loginform {
             margin: 0 auto;
             padding-bottom: 25px;
             background: #EBF4FB;
@@ -127,8 +129,8 @@ if (isset ( $_GET ['logout'] )) {
 </head>
 <body>
 <?php
-if (! isset ( $_SESSION ['name'] )) {
-    loginForm ();
+if (!isset ($_SESSION ['name'])) {
+    loginForm();
 } else {
     ?>
     <div id="wrapper">
@@ -136,46 +138,54 @@ if (! isset ( $_SESSION ['name'] )) {
             <p class="welcome">
                 Welcome, <b><?php echo $_SESSION['name']; ?></b>
             </p>
+
             <p class="logout">
                 <a id="exit" href="#">Exit Chat</a>
             </p>
+
             <div style="clear: both"></div>
         </div>
         <div id="chatbox"><?php
-            if (file_exists ( "log.html" ) && filesize ( "log.html" ) > 0) {
-                $handle = fopen ( "log.html", "r" );
-                $contents = fread ( $handle, filesize ( "log.html" ) );
-                fclose ( $handle );
+            if (file_exists("log.html") && filesize("log.html") > 0) {
+                $handle = fopen("log.html", "r");
+                $contents = fread($handle, filesize("log.html"));
+                fclose($handle);
 
                 echo $contents;
             }
             ?></div>
 
         <form name="message" action="">
-            <input name="usermsg" type="text" id="usermsg" size="63" /> <input
-                name="submitmsg" type="submit" id="submitmsg" value="Send" />
+            <input name="usermsg" type="text" id="usermsg" size="63"/> <input
+                name="submitmsg" type="submit" id="submitmsg" value="Send"/>
         </form>
     </div>
     <script type="text/javascript"
             src="http://ajax.googleapis.com/ajax/libs/jquery/1.3/jquery.min.js"></script>
     <script type="text/javascript">
+        window.onbeforeunload = function () {
+            var exit = confirm("Are you sure you want to end the session?");
+            if (exit == true) {
+                window.location = 'index.php?logout=true';
+            }
+        }
         // jQuery Document
-        $(document).ready(function(){
+        $(document).ready(function () {
         });
 
         //jQuery Document
-        $(document).ready(function(){
+        $(document).ready(function () {
             //If user wants to end session
-            $("#exit").click(function(){
+            $("#exit").click(function () {
                 var exit = confirm("Are you sure you want to end the session?");
-                if(exit==true){
+                if (exit == true) {
                     window.location = 'index.php?logout=true';
                 }
             });
         });
 
         //If user submits the form
-        $("#submitmsg").click(function(){
+        $("#submitmsg").click(function () {
             var clientmsg = $("#usermsg").val();
             $.post("post.php", {text: clientmsg});
             $("#usermsg").attr("value", "");
@@ -183,24 +193,24 @@ if (! isset ( $_SESSION ['name'] )) {
             return false;
         });
 
-        function loadLog(){
+        function loadLog() {
             var oldscrollHeight = $("#chatbox").attr("scrollHeight") - 20; //Scroll height before the request
             $.ajax({
                 url: "log.html",
                 cache: false,
-                success: function(html){
+                success: function (html) {
                     $("#chatbox").html(html); //Insert chat log into the #chatbox div
 
                     //Auto-scroll
                     var newscrollHeight = $("#chatbox").attr("scrollHeight") - 20; //Scroll height after the request
-                    if(newscrollHeight > oldscrollHeight){
-                        $("#chatbox").animate({ scrollTop: newscrollHeight }, 'normal'); //Autoscroll to bottom of div
+                    if (newscrollHeight > oldscrollHeight) {
+                        $("#chatbox").animate({scrollTop: newscrollHeight}, 'normal'); //Autoscroll to bottom of div
                     }
                 },
             });
         }
 
-        setInterval (loadLog, 2500);
+        setInterval(loadLog, 2500);
     </script>
     <?php
 }
